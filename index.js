f2f3e8cf1e86d86329a7696d966d55111c8c6386
Marwa <koroko123430@gmail.com>
2022-09-19 00:41:30 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const INPUT = document.querySelector('input')
+const WRITEoRDER = document.querySelector('.writeOrder')
+const RESULT = document.querySelector('.result')
+const MADA7 = document.querySelector('h4')
+const QUESTIONdev = document.querySelector('.Q')
+const QUESTION = document.querySelector('.question')
+const GENDERbtn = document.querySelector('#genderBtn')
+
+let gender = "female"
+document.querySelector('#send').addEventListener('click', () => {
+    if (INPUT.value == '') {
+        WRITEoRDER.classList.remove('hide');
+        RESULT.classList.add('hide');
+    } else {
+        const word = words[Math.floor(Math.random() * words.length)]
+        console.log(words.length);
+        WRITEoRDER.classList.add('hide');
+        QUESTIONdev.classList.add('hide');
+        MADA7.innerHTML = word;
+        RESULT.classList.remove('hide');
+    }
+})
+document.querySelector('#back').addEventListener('click', () => {
+    QUESTIONdev.classList.remove('hide');
+    RESULT.classList.add('hide');
+    INPUT.value = "";
+})
+
+GENDERbtn.addEventListener('click', () => {
+    gender = (gender == "female") ? "male" : "female"
+    appTexts(gender)
+})
+
+const appTexts = (gender = "female") => {
+    let pronoun = (gender == "female") ? fWords : mWords
+    QUESTION.innerHTML = pronoun.question;
+    WRITEoRDER.innerHTML = pronoun.writeOrder;
+    GENDERbtn.innerHTML = pronoun.gender;
+    words = pronoun.mad7
+}
+appTexts()
